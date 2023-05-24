c3fc6397cc1bcec9c860574c4c8fdacff3b71733
Hiten <hitenchawda24@gmail.com>
2023-05-24 22:29:18 +0530

    Implemented NavigationDrawer

--- app/src/main/java/com/hcapps/journal/navigation/NavGraph.kt
+++ app/src/main/java/com/hcapps/journal/navigation/NavGraph.kt
@@ -1,8 +1,14 @@
 package com.hcapps.journal.navigation
 
 import android.widget.Toast
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
@@ -11,10 +17,17 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.hcapps.journal.presentation.components.DisplayAlterDialog
 import com.hcapps.journal.presentation.screens.auth.AuthenticationScreen
 import com.hcapps.journal.presentation.screens.auth.AuthenticationViewModel
+import com.hcapps.journal.presentation.screens.home.HomeScreen
+import com.hcapps.journal.util.Constants.APP_ID
 import com.hcapps.journal.util.Constants.WRITE_SCREEN_ARGUMENT_KEY
 import com.stevdzasan.onetap.rememberOneTapSignInState
+import io.realm.kotlin.mongodb.App
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun SetupNavGraph(startDestination: String, navController: NavHostController) {
@@ -26,7 +39,15 @@ fun SetupNavGraph(startDestination: String, navController: NavHostController) {
             navController.popBackStack()
             navController.navigate(Screen.Home.route)
         })
-        homeRoute()
+        homeRoute(
+            navigateToWrite = {
+                navController.navigate(Screen.Write.route)
+            },
+            navigateToAuth = {
+                navController.popBackStack()
+                navController.navigate(Screen.Authentication.route)
+            }
+        )
         writeRoute()
     }
 }
@@ -70,8 +91,41 @@ fun NavGraphBuilder.authenticationRoute(navigateToHome: () -> Unit) {
     }
 }
 
-fun NavGraphBuilder.homeRoute() {
+fun NavGraphBuilder.homeRoute(
+    navigateToWrite: () -> Unit,
+    navigateToAuth: () -> Unit
+) {
     composable(route = Screen.Home.route) {
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        var signOutDialogOpened by remember { mutableStateOf(false) }
+        val scope = rememberCoroutineScope()
+        HomeScreen(
+            onMenuClicked = {
+                scope.launch {
+                    drawerState.open()
+                }
+            },
+            navigateToWrite = navigateToWrite,
+            drawerState = drawerState,
+            onSignOutClicked = {
+                signOutDialogOpened = true
+            }
+        )
+
+        DisplayAlterDialog(
+            title = "Sign Out",
+            message = "Are you sure you want to Sign Out from your Google Account",
+            dialogOpened = signOutDialogOpened,
+            onCloseDialog = { signOutDialogOpened = false },
+            onYesClicked = {
+                scope.launch(Dispatchers.IO) {
+                    App.create(APP_ID).currentUser?.logOut()
+                    withContext(Dispatchers.Main) {
+                        navigateToAuth()
+                    }
+                }
+            }
+        )
 
     }
 }
