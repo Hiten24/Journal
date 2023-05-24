214d0cc0afb22d0e9a01041033e1a0d5da209fba
Hiten <hitenchawda24@gmail.com>
2023-05-24 23:02:11 +0530

    created model class

---
+++ app/src/main/java/com/hcapps/journal/model/Journal.kt
@@ -0,0 +1,19 @@
+package com.hcapps.journal.model
+
+import androidx.room.PrimaryKey
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.RealmInstant
+import io.realm.kotlin.types.RealmList
+import io.realm.kotlin.types.RealmObject
+import org.mongodb.kbson.ObjectId
+
+class Journal: RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = ObjectId()
+    var ownerId: String = ""
+    var mood: String = Mood.Neutral.name
+    var title: String = ""
+    var description: String = ""
+    var images: RealmList<String> = realmListOf()
+    var date: RealmInstant = RealmInstant.from(System.currentTimeMillis(), 0)
+}
