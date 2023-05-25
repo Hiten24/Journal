9e107a75f261c651ff1763ec1216b9442d4555ed
Hiten <hitenchawda24@gmail.com>
2023-05-25 16:37:47 +0530

    created Gallery Component

--- app/src/main/java/com/hcapps/journal/presentation/components/JournalHolder.kt
+++ app/src/main/java/com/hcapps/journal/presentation/components/JournalHolder.kt
@@ -1,5 +1,6 @@
 package com.hcapps.journal.presentation.components
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -35,15 +36,17 @@ import com.hcapps.journal.model.Journal
 import com.hcapps.journal.model.Mood
 import com.hcapps.journal.ui.theme.Elevation
 import com.hcapps.journal.util.toInstant
+import io.realm.kotlin.ext.realmListOf
 import java.text.SimpleDateFormat
 import java.time.Instant
 import java.util.Date
 import java.util.Locale
 
 @Composable
-fun DiaryHolder(journal: Journal, onClick: (String) -> Unit) {
+fun JournalHolder(journal: Journal, onClick: (String) -> Unit) {
     var componentHeight by remember { mutableStateOf(0.dp) }
     val localDensity = LocalDensity.current
+    var galleryOpened by remember { mutableStateOf(false) }
 
     Row(
         modifier = Modifier
@@ -79,6 +82,16 @@ fun DiaryHolder(journal: Journal, onClick: (String) -> Unit) {
                     maxLines = 4,
                     overflow = TextOverflow.Ellipsis
                 )
+                if (journal.images.isNotEmpty()) {
+                    ShowGalleryButton(galleryOpened = galleryOpened) {
+                        galleryOpened = !galleryOpened
+                    }
+                }
+                AnimatedVisibility(visible = galleryOpened) {
+                    Column(modifier = Modifier.padding(all = 14.dp)) {
+                        Gallery(images = journal.images)
+                    }
+                }
             }
         }
     }
@@ -117,23 +130,28 @@ fun JournalHeader(moodName: String, time: Instant) {
 
 @Preview
 @Composable
-fun DiaryHolderPreview() {
-    DiaryHolder(journal = Journal().apply {
-        title = "My Journal"
-        description = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n" +
-                "\n" +
-                "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\n" +
-                "\n" +
-                "Where can I get some?\n" +
-                "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.\n" +
-                "\n" +
-                "5\n" +
-                "\tparagraphs\n" +
-                "\twords\n" +
-                "\tbytes\n" +
-                "\tlists\n" +
-                "\tStart with 'Lorem\n" +
-                "ipsum dolor sit amet...'\n"
-        mood = Mood.Happy.name
-    }, onClick = {})
+fun JournalHolderPreview() {
+    JournalHolder(
+        journal = Journal().apply {
+            title = "My Journal"
+            description =
+                "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n" +
+                        "\n" +
+                        "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\n" +
+                        "\n" +
+                        "Where can I get some?\n" +
+                        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.\n" +
+                        "\n" +
+                        "5\n" +
+                        "\tparagraphs\n" +
+                        "\twords\n" +
+                        "\tbytes\n" +
+                        "\tlists\n" +
+                        "\tStart with 'Lorem\n" +
+                        "ipsum dolor sit amet...'\n"
+            mood = Mood.Happy.name
+            images = realmListOf("", "", "", "", "", "")
+        },
+        onClick = {}
+    )
 }
