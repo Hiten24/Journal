84155bec3023c8a4d842fece5491cfdd47720c5b
Hiten <hitenchawda24@gmail.com>
2023-07-11 15:39:50 +0530

    updated splash icon

--- app/src/main/java/com/hcapps/journal/MainActivity.kt
+++ app/src/main/java/com/hcapps/journal/MainActivity.kt
@@ -3,19 +3,16 @@ package com.hcapps.journal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.FirebaseApp
 import com.hcapps.journal.navigation.SetupNavGraph
-import com.hcapps.mongo.database.ImageToUploadDao
 import com.hcapps.ui.theme.JournalTheme
 import com.hcapps.util.Constants.APP_ID
 import com.hcapps.util.Screen
 import dagger.hilt.android.AndroidEntryPoint
 import io.realm.kotlin.mongodb.App
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -25,7 +22,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        installSplashScreen()
+        installSplashScreen()
         WindowCompat.setDecorFitsSystemWindows(window, false)
         FirebaseApp.initializeApp(this)
         setContent {
@@ -42,7 +39,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-private fun cleanupCheck(
+/*private fun cleanupCheck(
     scope: CoroutineScope,
     imageToUploadDao: ImageToUploadDao
 ) {
@@ -54,7 +51,7 @@ private fun cleanupCheck(
             }
         }
     }
-}
+}*/
 
 private fun getStartDestination(): String {
     val user = App.create(APP_ID).currentUser
